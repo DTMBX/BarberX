ab428868c5d5fb2ac2b69b1411446294431653d3
Evident CI <ci@evident.icu>
2026-02-15 02:12:42 -0500

    Squashed 'apps/epstein-library-evid/' content from commit bd41de509
    git-subtree-dir: apps/epstein-library-evid
    git-subtree-split: bd41de5096f5c1765f7e8132927371bcbfc0dba7

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client'
+import { ErrorBoundary } from "react-error-boundary";
+import "@github/spark/spark"
+
+import App from './App.tsx'
+import { ErrorFallback } from './ErrorFallback.tsx'
+import { EngineProvider } from './hooks/use-engine.tsx'
+
+import "./main.css"
+import "./styles/theme.css"
+import "./index.css"
+
+createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <EngineProvider>
+      <App />
+    </EngineProvider>
+   </ErrorBoundary>
+)
